b439fe6d73a40f3d7993a2358049f74b8165e4ef
musash1 <luis.gjokaj@proton.me>
2024-12-03 21:03:52 +0100

    day3 part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, usize};
 
 use regex::Regex;
 
@@ -6,8 +6,7 @@ fn main() {
     let file = fs::read_to_string("text.txt").expect("Not able to read File.");
 
     println!("Part 1: {:?}", part1(&file));
-    println!("Part 2: {:?}", part2(&file));
-}
+    println!("Part 2: {:?}", part2(&file)); }
 
 fn check_mul(input: &str) -> Vec<&str> {
     let re = Regex::new(r"mul\(([+-]?\d+(?:\.\d+)?),([+-]?\d+(?:\.\d+)?)\)").unwrap();
@@ -44,5 +43,33 @@ fn part1(input: &str) -> i32 {
 }
 
 fn part2(input: &str) -> i32 {
-    0
+    let mut result = 0;
+    let mut temp: String = input.to_string();
+
+    loop {
+        let dont_index = temp.find("don't()").unwrap_or(usize::MAX);
+        let do_index = temp.find("do()").unwrap_or(usize::MAX);
+
+        if do_index < dont_index || (dont_index == usize::MAX && do_index != usize::MAX) {
+            temp.replace_range(do_index..(do_index + 3), "");
+            continue;
+        }
+
+        if dont_index == usize::MAX && do_index == usize::MAX {break;}
+        else if dont_index != usize::MAX && do_index == usize::MAX {
+            temp.replace_range(dont_index..(temp.len() - 1), "");
+            break;
+        } else if dont_index != usize::MAX && do_index != usize::MAX {
+            temp.replace_range(dont_index..(do_index + 3), "");
+        }
+    }
+
+    let checked_input = check_mul(&temp);
+
+    for mul in checked_input {
+        let comma_pos = get_comma(mul);
+        result += mul.get(4..comma_pos).unwrap().parse::<i32>().unwrap() * mul.get((comma_pos + 1)..(mul.len() - 1)).unwrap().parse::<i32>().unwrap();
+    }
+    
+    result
 }
